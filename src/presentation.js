36acc407d794e2a4515d0992a3bf9e9ee9d37e15
Kadi Kraman <hellokadi@gmail.com>
2017-12-10 15:47:15 +0000

    Add the first 4 slides

--- src/presentation.js
+++ src/presentation.js
@@ -1,5 +1,5 @@
 // Import React
-import React from 'react';
+import React from "react";
 
 // Import Spectacle Core tags
 import {
@@ -7,83 +7,78 @@ import {
   Cite,
   Deck,
   Heading,
-  ListItem,
-  List,
   Quote,
   Slide,
   Text,
-} from 'spectacle';
+  Image
+} from "spectacle";
 
 // Import theme
-import createTheme from 'spectacle/lib/themes/default';
+import createTheme from "spectacle/lib/themes/default";
+
+import reactNativeMobileSrc from "./assets/mobile.jpg";
+import reactLoveSrc from "./assets/reactLove.png";
 
 // Require CSS
-require('normalize.css');
+require("normalize.css");
 
 const theme = createTheme(
   {
-    primary: 'white',
-    secondary: '#1F2022',
-    tertiary: '#03A9FC',
-    quartenary: '#CECECE',
+    primary: "white",
+    black: "#1F2022",
+    christmasRed: "#992114",
+    christmasYellow: "#C5A436",
+    christmasGreen: "#176100",
+    white: "#FFFFFF",
+    blue: "#10a3de"
   },
   {
-    primary: 'Montserrat',
-    secondary: 'Helvetica',
+    primary: "Montserrat",
+    secondary: "Helvetica"
   }
 );
 
 export default class Presentation extends React.Component {
   render() {
     return (
-      <Deck
-        transition={['zoom', 'slide']}
-        transitionDuration={500}
-        theme={theme}
-      >
-        <Slide transition={['zoom']} bgColor="primary">
-          <Heading size={1} fit caps lineHeight={1} textColor="secondary">
-            Spectacle Boilerplate
+      <Deck transition={["fade"]} transitionDuration={500} theme={theme}>
+        <Slide bgColor="christmasRed">
+          <Heading fit caps lineHeight={1} textColor="white">
+            React to React Native
           </Heading>
-          <Text margin="10px 0 0" textColor="tertiary" size={1} fit bold>
-            open the presentation/index.js file to get started
+          <Text size={4} textColor="christmasYellow" bold>
+            How Hard Could It Be?
           </Text>
+          <div style={{ marginTop: "50px" }}>
+            <Text size={6} textColor="white">
+              Kadi Kraman
+            </Text>
+            <Text size={6} textColor="white">
+              Formidable Labs
+            </Text>
+          </div>
         </Slide>
-        <Slide transition={['fade']} bgColor="tertiary">
-          <Heading size={6} textColor="primary" caps>
-            Typography
+        <Slide bgColor="christmasGreen" textColor="white">
+          <Heading size={4} textColor="white" caps>
+            React - game changer for the web!
           </Heading>
-          <Heading size={1} textColor="secondary">
-            Heading 1
-          </Heading>
-          <Heading size={2} textColor="secondary">
-            Heading 2
-          </Heading>
-          <Heading size={3} textColor="secondary">
-            Heading 3
-          </Heading>
-          <Heading size={4} textColor="secondary">
-            Heading 4
-          </Heading>
-          <Heading size={5} textColor="secondary">
-            Heading 5
-          </Heading>
-          <Text size={6} textColor="secondary">
-            Standard text
-          </Text>
         </Slide>
-        <Slide transition={['fade']} bgColor="primary" textColor="tertiary">
-          <Heading size={6} textColor="secondary" caps>
-            Standard List
-          </Heading>
-          <List>
-            <ListItem>Item 1</ListItem>
-            <ListItem>Item 2</ListItem>
-            <ListItem>Item 3</ListItem>
-            <ListItem>Item 4</ListItem>
-          </List>
+        <Slide bgColor="christmasRed" textColor="white">
+          <Image src={reactLoveSrc} />
+          <Text textColor="white">Lightweight fontend-only framework</Text>
+          {/* As opposed to e.g Angular.js, concerned with JUST the frontend and nothing else */}
+          <Text textColor="white">Component-based architecture</Text>
+          {/* Promoting resusability, reducing dev time of new features as project goes on */}
+          <Text textColor="white">Composition over inheritance</Text>
+          {/* Embracing functional programming methodology */}
         </Slide>
-        <Slide transition={['fade']} bgColor="secondary" textColor="primary">
+        <Slide bgColor="blue" textColor="white">
+          <Heading size={4} textColor="white" caps>
+            React Native - game changer for mobile?
+          </Heading>
+          <Image src={reactNativeMobileSrc} />
+        </Slide>
+        <Slide bgColor="black" textColor="white">
           <BlockQuote>
             <Quote>Example Quote</Quote>
             <Cite>Author</Cite>
